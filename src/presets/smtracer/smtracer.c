8130ec0ab1709f395ff9e6bdb5d0a46990759021
Simone Economo <simone.economo@gmail.com>
2016-05-18 11:31:04 +0200

    Ho implementato delle modifiche all'smtracer per tener conto degli accessi effettuati nei blocchi non selezionati dalla discplina di instrumentazione quando si è in modalità simulata. Questo dovrebbe completare le modifiche al tracer, a meno dell'insorgere di qualche bug e di dover modificare l'euristica di selezione degli accessi.

--- src/presets/smtracer/smtracer.c
+++ src/presets/smtracer/smtracer.c
@@ -1594,6 +1594,7 @@ static size_t smt_instrument_func(function *func, symbol *callfunc) {
     // Free unnecessary heap memory
     for (access = smt->candidates; access; access = temp) {
       temp = access->next;
+
       free(access);
     }
   }
